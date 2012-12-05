0c5271d333e47ebb6b9ba7bc3f24b3b412c07667
darcyclarke <darcy@darcyclarke.me>
2012-12-05 11:45:09 -0500

    2.0 Overhaul WIP
    * Stored 1.0 version of detect under /builds/1.0.0/
    * Modified readme documentation and examples
    * Added .gitignore
    * Added node.js build tool in /build/build.js
    * Moved regex profiles out of base detect.js and in to /build/regexes.js
    * Deprecated .toString and .toVersionStrin
    * Added .name and .version to browser, os and device objects
    * Rescoped methods/variables and included new internal utilities
    * Added .setParsers, .setFamilies, .addParsers, .addFamilies and .filterParsers methods

---
+++ build/build.js
@@ -0,0 +1,38 @@
+// Setup
+var detect			= require('../detect');
+var regexes			= require('../build/regexes');
+var util				= require('util');
+
+var command			= (process.argv[2]) ? process.argv[2] : false,
+		type				= (process.argv[3]) ? process.argv[3] : false,
+		arguments		= (process.argv[4]) ? process.argv[4].split(' ') : false,
+		output			= [],
+		error				= false;
+
+if(!command){
+	output.push('Error: No command passed');
+	error = true;
+}
+
+if(!type){
+	output.push('Error: No type passed');
+	error = true;
+}
+
+if(!arguments){
+	output.push('Error: No arguments passed');
+	error = true;
+}
+
+// Set Parsers
+detect.setParsers(regexes);
+
+// Set Families
+detect.setFamilies(arguments);
+
+// Filter Parsers & Pass to Output
+// output.push();
+
+// Output
+// util.puts(output.join('\n'));
+console.log(detect.filterParsers());
